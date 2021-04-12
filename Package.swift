079eda238b55d0d7a1b85ab2f0a91539ca3a3579
Shanbin.Cai <shanbin.cai@gmail.com>
2021-04-12 11:52:08 +0800

    add products

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "ApplyStyleKit",
+    products: [
+        .executable(name: "ApplyStyleKit", targets: ["ApplyStyleKit"])
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
